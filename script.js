38d852d18b27b393962688446b6743e11d6f25f3
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-01-24 15:24:49 -0800

    primitive js list

---
+++ script.js
@@ -0,0 +1,18 @@
+const groceryItemFromInput = document.querySelector("input");
+const addButton = document.querySelector("button");
+
+function addNewGroceryItem() {
+    const itemToAdd= groceryItemFromInput.value;
+    if (itemToAdd) {
+        const newGroceryItem = document.createElement("li");
+        newGroceryItem.textContent = itemToAdd;
+        document.querySelector("ul").appendChild(newGroceryItem);
+        groceryItemFromInput.value = "";
+    }
+}
+
+addButton.addEventListener('click', addNewGroceryItem);
+
+
+
+
