a333f88f76fe73da1810b579e51b46e0c901eb58
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-01-27 20:18:31 -0800

    add jan-27 class examples

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-const groceryItemFromInput = document.querySelector("input");
-const addButton = document.querySelector("button");
+// if - else, switches, loop: for loop, while
 
-function addNewGroceryItem() {
-    const itemToAdd= groceryItemFromInput.value;
-    if (itemToAdd) {
-        const newGroceryItem = document.createElement("li");
-        newGroceryItem.textContent = itemToAdd;
-        document.querySelector("ul").appendChild(newGroceryItem);
-        groceryItemFromInput.value = "";
-    }
+
+const fruit = '';
+
+switch (fruit) {
+    case 'Apple':
+        console.log('🍎');
+        break;
+    case 'Lemon':
+        console.log('🍋');
+        break;
+    default:
+        console.log('🍊')
 }
 
-addButton.addEventListener('click', addNewGroceryItem);
+// const outputText = (a > b) ? 'from if' : 'text from else';
+
+// console.log(outputText)
 
 
 
-callMyDoctor('headache')
 
-function sadsadsad (problem) {
-    const HARD_CODED_NUMBER = '236-777-8888';
-    console.log(HARD_CODED_NUMBER, problem)
+
+
+function outputB (parameterB) {
+    console.log('hi from function')
+    console.log(parameterB)
+
 }
 
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
 
 
 
-callMyDoctor('headache')
 
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code lines
-// actual code linesç
-callMyDoctor('headache')
-callMyDoctor('headache')
+
+
+
+
+
+
+
+// const groceryItemFromInput = document.querySelector("input");
+// const addButton = document.querySelector("button");
+// callMyDoctor('headache')
+
+
+// function addNewGroceryItem() {
+//     const itemToAdd= groceryItemFromInput.value;
+//     if (itemToAdd) {
+//         const newGroceryItem = document.createElement("li");
+//         newGroceryItem.textContent = itemToAdd;
+//         document.querySelector("ul").appendChild(newGroceryItem);
+//         groceryItemFromInput.value = "";
+//     }
+// }
+
+// addButton.addEventListener('click', addNewGroceryItem);
+
+
+
+
+
+// function callMyDoctor (problem) {
+//     const HARD_CODED_NUMBER = '236-777-8888';
+//     console.log(HARD_CODED_NUMBER, problem)
+// }
+
+
